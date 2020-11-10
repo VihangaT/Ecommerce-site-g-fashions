1754b6006222e9234981e76cb3bbe3b26416d2a1
VihangaT <liyanage.vihanga99@gmail.com>
2020-11-11 02:03:43 +0530

    redux

---
+++ backend/data.js
@@ -0,0 +1,68 @@
+const data ={
+    products:[
+        {
+            _id:'1',
+            Name:'Nike Frock',
+            category:'frocks',
+            image : '/images/p1.jpg',
+            price:120,
+            countInStock:0,
+            brand:'Nike',
+            rating:4.5,
+            numReviews:10,
+            description:'high quality product'
+        },
+        {
+            _id:'2',
+            Name:'Jadon Frock',
+            category:'Jadon',
+            image : '/images/p2.jpg',
+            price:100,
+            countInStock:10,
+            brand:'Jadon',
+            rating:4.2,
+            numReviews:12,
+            description:'high quality garment'
+        },
+        {
+            _id:'3',
+            Name:'G-Fashion Frock',
+            category:'G-Fashion',
+            image : '/images/p3.jpg',
+            price:90,
+            countInStock:20,
+            brand:'G-Fashion',
+            rating:5,
+            numReviews:15,
+            description:'high quality garment'
+        },
+        {
+            _id:'4',
+            Name:'Fashion Frock',
+            category:'SL Cloth',
+            image : '/images/p4.jpeg',
+            price:90,
+            countInStock:15,
+            brand:'SL Cloth',
+            rating:4.3,
+            numReviews:16,
+            description:'High Quality'
+        },
+        {
+            _id:'5',
+            Name:'Addidas Shirt',
+            category:'Addidas',
+            image : '/images/p5.jpg',
+            price:290,
+            countInStock:10,
+            brand:'Addidas',
+            rating:2,
+            numReviews:14,
+            description:'Imported Hight quality garment'
+        }
+        
+
+    ]
+}
+
+export default data;
